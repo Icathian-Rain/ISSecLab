045dfa1b5e541e22a5f282ba31e69bcd4cf05f9b
icathianrain <2205794866@qq.com>
2023-04-21 09:33:48 +0800

    init(homework1) : 初始化homework1

---
+++ homework1/stack.c
@@ -0,0 +1,31 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dlfcn.h>
+
+void start() {
+  printf("IOLI Crackme Level 0x00\n");
+  printf("Password:");
+
+  char buf[64];
+  memset(buf, 0, sizeof(buf));
+  read(0, buf, 256);
+
+  if (!strcmp(buf, "250382"))
+    printf("Password OK :)\n");
+  else
+    printf("Invalid Password!\n");
+}
+
+int main(int argc, char *argv[]) {
+
+  setreuid(geteuid(), geteuid());
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF,0);
+
+  start();
+
+  return 0;
+}
