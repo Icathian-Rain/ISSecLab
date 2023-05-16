f99cff94cd754a27b441aef6d04a04ec4bda4d60
icathianrain <2205794866@qq.com>
2023-05-16 19:04:10 +0800

    init lab1

---
+++ lab1/src/prog2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void win() {
+    puts("You Win!");
+}
+
+void fmtstr(char* str)
+{
+    unsigned int *framep;
+    unsigned int *ret;
+    //copy ebp into framep
+    asm("movl %%ebp, %0" : "=r" (framep));
+    ret = framep + 1;
+   
+    /* print out information for experiment purpose */
+    printf("The address of the input array: 0x%.8x\n", (unsigned)str);
+    printf("The value of the frame pointer: 0x%.8x\n", (unsigned)framep);
+    printf("The value of the return address(before): 0x%.8x\n", *ret);
+
+    printf(str); 
+
+    printf("\nThe value of the return address(after): 0x%.8x\n", *ret);
+}
+
+int main() 
+{ 
+    FILE *badfile;    
+    char str[200];
+
+    badfile = fopen("input", "rb");
+    fread(str, sizeof(char), 200, badfile);
+    fmtstr(str);
+    return 1; 
+}
